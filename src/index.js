5b0f4a2c8cada26193795ec50a49ab60f8ae0a77
Zhenzhen Zhao <229194950@qq.com>
2024-07-26 17:05:13 +0000

    add triplez

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.proxy.triplez.cn": dockerHub,
+  "quay.proxy.triplez.cn": "https://quay.io",
+  "gcr.proxy.triplez.cn": "https://gcr.io",
+  "k8s-gcr.proxy.triplez.cn": "https://k8s.gcr.io",
+  "k8s.proxy.triplez.cn": "https://registry.k8s.io",
+  "ghcr.proxy.triplez.cn": "https://ghcr.io",
+  "cloudsmith.proxy.triplez.cn": "https://docker.cloudsmith.io",
+  "ecr.proxy.triplez.cn": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.proxy.triplez.cn": dockerHub,
 };
 
 function routeByHosts(host) {
